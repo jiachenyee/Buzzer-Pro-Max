fc0cd59378442ae894d63a087a82e74c66a380b8
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-03-08 21:44:49 +0800

    Initial Commit

---
+++ Shared/Buzzer_Pro_MaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Buzzer_Pro_MaxApp.swift
+//  Shared
+//
+//  Created by Jia Chen Yee on 8/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Buzzer_Pro_MaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
