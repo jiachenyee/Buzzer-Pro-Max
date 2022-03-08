8c05b87e1a9504c4d55e8f0e9f00b55776775d4b
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-03-08 22:11:10 +0800

    initial project setup; add app icons

---
+++ iOS/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Jia Chen Yee on 8/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100)
+                Text("Swift Innovators’ Summit 2022")
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding()
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- macOS/ContentView.swift
+++ macOS/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Shared
+//  Buzzer Pro Max (macOS)
 //
 //  Created by Jia Chen Yee on 8/3/22.
 //
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
