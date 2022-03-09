30c5ff3af5907be50a4f88480943ecd65739da16
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-03-09 21:00:45 +0800

    RUI AYNG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ iOS/CommunicationManager.swift
@@ -0,0 +1,18 @@
+//
+//  CommunicationManager.swift
+//  Buzzer Pro Max (iOS)
+//
+//  Created by Jia Chen Yee on 9/3/22.
+//
+
+import Foundation
+import SwiftUI
+import MultipeerConnectivity
+
+class CommunicationManager: ObservableObject {
+    
+    
+    func send(message: Messagable) {
+        #warning("Rui Yang look here")
+    }
+}
