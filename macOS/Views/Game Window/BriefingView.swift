4944b6aca1725886b79445a7de58c739bdbc0011
ThinkerPal <thinkerpal@gmail.com>
2022-03-15 01:23:31 +0800

    Added Briefing Slides to Assets folder

---
+++ macOS/Views/Game Window/BriefingView.swift
@@ -0,0 +1,20 @@
+//
+//  BriefingView.swift
+//  Buzzer Pro Max (macOS)
+//
+//  Created by Rui Yang on 15/3/22.
+//
+
+import SwiftUI
+
+struct BriefingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BriefingView_Previews: PreviewProvider {
+    static var previews: some View {
+        BriefingView()
+    }
+}
